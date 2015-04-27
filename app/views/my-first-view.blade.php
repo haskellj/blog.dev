93dccbda7fc3f974b39a1600d52a5720cd4fe19f
Jamie Haskell <jmehaskell@gmail.com>
2015-04-27 15:38:10 -0500

    now uses blade echo short code

--- app/views/my-first-view.blade.php
+++ app/views/my-first-view.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
 @section('content')
 			{{-- Note: no semi-scolons after blade commands --}}
-    <h1>Hello, <?= $name; ?>!</h1>
-    <p>Here's something else: <?= $another; ?></p>
+    <h1>Hello, {{{$name}}}!</h1>
+    <p>Here's something else: {{{$another}}}</p>
 @stop
