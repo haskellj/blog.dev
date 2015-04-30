c7c6e78b2e2f2c14125424c85fe51ec549a5505e
Jamie Haskell <jmehaskell@gmail.com>
2015-04-30 10:23:26 -0500

    refactored form to display errors

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.master')
 
 @section('create-post')
-	<form method='POST' action="{{{ action('PostsController@store') }}}">
+	<form id="form" method='POST' action="{{{ action('PostsController@store') }}}">
+		{{ $errors->first('title', '<span class="help-block error">:message</span>') }}
 		<input type='text' name='title' value="{{{ Input::old('title') }}}" placeholder='Title'>
+		{{ $errors->first('body', '<span class="help-block error">:message</span>') }}
 		<textarea type='text' name='body' placeholder='Body'>{{{ Input::old('body') }}}</textarea>
 		<input type='submit' name='create' value='Post'>
 	</form>
