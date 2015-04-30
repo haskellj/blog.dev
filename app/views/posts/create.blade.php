4a0e352ab6de11e1c6b75278c88f32d2f42212f8
Jamie Haskell <jmehaskell@gmail.com>
2015-04-30 13:09:57 -0500

    added pagination to index.blade and debugger bar to Laravel

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -1,11 +1,14 @@
 @extends('layouts.master')
 
 @section('create-post')
-	<form id="form" method='POST' action="{{{ action('PostsController@store') }}}">
-		{{ $errors->first('title', '<span class="help-block error">:message</span>') }}
-		<input type='text' name='title' value="{{{ Input::old('title') }}}" placeholder='Title'>
-		{{ $errors->first('body', '<span class="help-block error">:message</span>') }}
-		<textarea type='text' name='body' placeholder='Body'>{{{ Input::old('body') }}}</textarea>
-		<input type='submit' name='create' value='Post'>
-	</form>
+	<fieldset>
+		<legend><h3>Create a New Post</h3></legend>
+			<form id="form" method='POST' action="{{{ action('PostsController@store') }}}">
+				{{ $errors->first('title', '<span class="help-block error">:message</span>') }}
+				<input type='text' name='title' value="{{{ Input::old('title') }}}" placeholder='Title'>
+				{{ $errors->first('body', '<span class="help-block error">:message</span>') }}
+				<textarea type='text' name='body' placeholder='Body'>{{{ Input::old('body') }}}</textarea>
+				<input type='submit' class='button round' name='create' value='Post'>
+			</form>
+	</fieldset>
 @stop
--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -2,15 +2,26 @@
 
 @section('all-posts')
 	<h1>All Posts: </h1>
+
+	{{-- Pagination Links --}}
+	<div class="pagination-centered">
+		{{ $posts->links() }}
+	</div>
+
 	@foreach($posts as $post)
 		<div class='row'>
 			<div class='large-6 columns'>
 				<div class='panel'>
-					<h3>{{{ $post->title }}}</h3>
+					<h3><a href="{{{ action('PostsController@show', $post->id) }}}">{{{ $post->title }}}</a></h3>
 					<h5>{{{ $post->body }}}</h5>
 					<p>{{{ $post->created_at }}}</p>
 				</div>
 			</div>
 		</div>
 	@endforeach
+
+	{{-- Pagination Links --}}
+	<div class="pagination-centered">
+		{{ $posts->links() }}
+	</div>
 @stop
