52f1035e3364672c6c209ee11d1e974378d2f83f
Jamie Haskell <jmehaskell@gmail.com>
2015-05-04 10:12:24 -0500

    added logic to make form more generic, it can now handle both creations and edits of posts

--- app/views/posts/edit.blade.php
+++ app/views/posts/edit.blade.php
@@ -2,30 +2,34 @@
 
 @section('post-form')
 {{-- {{ Route::currentRouteAction() }} --}}
-	<fieldset>
-			@if($method=="edit")
-				<legend><h3>Edit Post</h3></legend>
-				<form id="form" method='POST' action="{{{ action('PostsController@update', $id) }}}">
-					<input type="hidden" name="_method" value="PUT">
-			@else
-				<legend><h3>Create a New Post</h3></legend>
-				<form id="form" method='POST' action="{{{ action('PostsController@store') }}}">
-					<input type="hidden" name="_method" value="POST">
-			@endif
-					{{ $errors->first('title', '<span class="help-block error">:message</span>') }}
-					<input type='text' name='title' value="{{{$processedInput['title']}}}" placeholder="Title">
-					{{ $errors->first('body', '<span class="help-block error">:message</span>') }}
-					<textarea type='text' name='body' placeholder='Body'>{{{$processedInput['body']}}}</textarea>
-
-					@if($method="edit")
-						<input type='submit' class='button round' name='edit' value='Save Changes'>
+	<div class='row'>
+		<div class='large-8 medium-8 large-centered medium-centered columns'>
+			<fieldset>
+					@if($method=="edit")
+						<legend><h3>Edit Post</h3></legend>
+						<form id="form" method='POST' action="{{{ action('PostsController@update', $id) }}}">
+							<input type="hidden" name="_method" value="PUT">
 					@else
-						<input type='submit' class='button round' name='create' value='Post'>
+						<legend><h3>Create a New Post</h3></legend>
+						<form id="form" method='POST' action="{{{ action('PostsController@store') }}}">
+							<input type="hidden" name="_method" value="POST">
 					@endif
+							{{ $errors->first('title', '<span class="help-block error">:message</span>') }}
+							<input type='text' name='title' value="{{{$processedInput['title']}}}" placeholder="Title">
+							{{ $errors->first('body', '<span class="help-block error">:message</span>') }}
+							<textarea type='text' name='body' rows='10' cols='20' placeholder='Body'>{{{$processedInput['body']}}}</textarea>
 
-			{{-- Add CSRF protection, which is enabled in BaseController --}}
-			{{ Form::token() }}
+							@if($method=="edit")
+								<input type='submit' class='button round' name='edit' value='Save Changes'>
+							@else
+								<input type='submit' class='button round' name='create' value='Post'>
+							@endif
 
-			</form>
-	</fieldset>
+					{{-- Add CSRF protection, which is enabled in BaseController --}}
+					{{ Form::token() }}
+
+					</form>
+			</fieldset>
+		</div>
+	</div>
 @stop
