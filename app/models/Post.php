c88dddf91e27cae9322131d7026d1361c6ff9f60
Jamie Haskell <jmehaskell@gmail.com>
2015-05-04 11:46:17 -0500

    added a Mutator method to sluggify all input for the slug field before storing in the database

--- app/models/Post.php
+++ app/models/Post.php
@@ -11,6 +11,14 @@
 		];
 
 
+		// Store all post titles as a sluggified version of themselves in the 'slug' column
+		// This will create a duplicate of each post's title, connected by hyphens
+		// for use as a url appendage to direct link to each post (i.e. "This Blog Title" will also be stored as "this-blog-title")
+		public function setSlugAttribute($value)
+		{
+		    $this->attributes['slug'] = Str::slug($value);
+		}
 
 	}
+
 
