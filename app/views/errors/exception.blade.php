2aaf7ff3a0bfacca6d66cae635c1769188de1b0f
Jamie Haskell <jmehaskell@gmail.com>
2015-05-04 11:14:37 -0500

    just edited text

--- app/views/errors/exception.blade.php
+++ app/views/errors/exception.blade.php
@@ -3,7 +3,7 @@
 @section('error')
 	<div class='row'>
 		<div class='large-8 large-centered text-center columns'>
-			<h2>{{{ "Error 404:" }}}</h2>
+			<h2>{{{ "Oh Noes: 404" }}}</h2>
 			<hr>
 			<h4>{{{ "The page you were looking for does not exist." }}}</h4>
 		</div>
