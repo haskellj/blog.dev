1f4dd237f1d6ea9a5477e27080c7a922f12e94dc
Jamie Haskell <jmehaskell@gmail.com>
2015-05-05 10:42:54 -0500

    created seeder classes and functions for posts and users tables

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+
 class DatabaseSeeder extends Seeder {
 
 	/**
@@ -11,7 +12,65 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('UserTableSeeder');
+		$this->call('PostsTableSeeder');
 	}
 
+}
+
+class UserTableSeeder extends Seeder {
+
+	// Fill db with users
+	public function run()
+	{
+		DB::table('users')->delete();
+
+		$user1 = new User;
+		$user1->username = 'guest';
+		$user1->email = 'guest@gmail.com';
+		$user1->password = $_ENV['USER_PASS'];
+		$user1->save();
+
+		$user2 = new User;
+		$user2->username = 'test';
+		$user2->email = 'test@gmail.com';
+		$user2->password = $_ENV['USER_PASS'];
+		$user2->save();
+
+		$user3 = new User;
+		$user3->username = 'jamie';
+		$user3->email = 'jamie@gmail.com';
+		$user3->password = $_ENV['USER_PASS'];
+		$user3->save();
+
+	}
+}
+
+class PostsTableSeeder extends Seeder {
+
+	// Fill db with users
+	public function run()
+	{
+		DB::table('posts')->delete();
+		$users = User::all();
+
+		// Start each user off with at least 1 post
+		foreach ($users as $user) {
+			$post = new Post;
+			$post->title = "Title " . $user->user_id;
+			$post->body = "Body " . $user->user_id;
+			$post->slug = $post->title;
+			$post->user_id = $user->user_id;
+			$post->save();
+		}
+		// Randomly assign 7 more posts to users
+		for($i = 4; $i <= 10; $i++) {
+			$post = new Post;
+			$post->title = "Title $i";
+			$post->body = "Body $i";
+			$post->slug = $post->title;
+			$post->user_id = rand(1, 3);
+			$post->save();
+		}
+	}
 }
