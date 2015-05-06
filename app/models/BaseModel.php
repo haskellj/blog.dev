6160a682f5f67793f7df87d808bc7d3b96082913
Jamie Haskell <jmehaskell@gmail.com>
2015-05-06 13:36:36 -0500

    changed Accessor comment

--- app/models/BaseModel.php
+++ app/models/BaseModel.php
@@ -6,7 +6,7 @@
 	{
 		protected $table;
 
-		// automatic date-formatter for all posts created_at date
+		// Accessor that automatically formats dates for all posts created_at date
 		// displays '24 minutes ago, 2 days ago, etc.'
 		// public function getCreatedAtAttribute($value)
 		// {
@@ -14,7 +14,7 @@
 		//     return $utc->setTimezone('America/Chicago')->diffForHumans();
 		// }
 	
-		// automatic date-formatter for all posts updated_at date
+		// Accessor that automatically formats dates for all posts updated_at date
 		// displays '24 minutes ago, 2 days ago, etc.'
 		public function getUpdatedAtAttribute($value)
 		{
