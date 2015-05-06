70854c1dca0fc512730c97e3740cf5efcc1dc1f2
Jamie Haskell <jmehaskell@gmail.com>
2015-05-06 13:25:40 -0500

    added a _construct() filter and login/out methods

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -15,26 +15,97 @@ class HomeController extends BaseController {
 	|
 	*/
 
+	public function __construct()
+	{
+		// first parameter is name of filter, the second is an array containing either 'only' or 'except' associated with its own array of HomeController methods that direct to certain pages
+		$this->beforeFilter('auth', array('only' => array('updateAll')));
+	}
+
 	public function showHome()
 	{
-		$data = array(
+		// this array will be used to add/remove classes to navlinks
+		$class = array(
 				'aboutActive' => "active",		// These keys will become variables in the View, much like the PHP extract() function
 				'resumeActive' => '',
 				'portfolioActive' => '',
 			);
-		return View::make('about')->with($data);
+		return View::make('home')->with($class);
 	}
 
 	public function showResume()
 	{
-		return View::make('resume');
+		// this array will be used to add/remove classes to navlinks
+		$class = array(
+				'aboutActive' => '',		// These keys will become variables in the View, much like the PHP extract() function
+				'resumeActive' => 'active',
+				'portfolioActive' => '',
+			);
+		return View::make('resume')->with($class);
 	}
 
 	public function showPortfolio()
 	{
-		return View::make('portfolio');
+		// this array will be used to add/remove classes to navlinks
+		$class = array(
+				'aboutActive' => '',		// These keys will become variables in the View, much like the PHP extract() function
+				'resumeActive' => '',
+				'portfolioActive' => 'active',
+			);
+		return View::make('portfolio')->with($class);
 	}
 
+//===================== Authentication Controllers ====================
+	// get route
+	public function login()
+	{
+		// dd(Auth::check());
+		//if logged in already, send them to the posts page
+		if(Auth::check()) {
+			Session::flash('errorFlash', 'Already logged in.');
+			return Redirect::to('/');
+		}
+		return View::make('users.login');
+	}
+
+	// post route
+	public function attemptLogin()
+	{	
+
+		// create the validator
+	    $validator = Validator::make(Input::all(), User::$rules);
+
+		$usernameOrEmail = Input::get('username_or_email');
+		$password = Input::get('password');
+
+		if ($validator->fails()) {
+			// temporary error message that will load on redirected page.
+	    	Session::flash('errorFlash', 'Field(s) submitted with no input.');
+	        // validation failed, redirect to the post edit page with validation errors and old inputs
+	    	return Redirect::to("login#form")->withInput()->withErrors($validator);
+
+		// If username/password or email/password are correct, direct wherever user intended to go, otherwise default to homepage
+		} elseif ((Auth::attempt(array('username' => $usernameOrEmail, 'password' => $password)))
+			|| Auth::attempt(array('email' => $usernameOrEmail, 'password' => $password))) {
+		    
+		    // dd(Auth::check());
+	    	Session::flash('successFlash', 'Login successful!');
+	    	return Redirect::intended('/');
+
+		} else {
+		    // login failed, go back to the login screen, display error message
+		    Session::flash('errorFlash', 'Login failed, username/email or password were incorect.');
+	    	return Redirect::to("login#form")->withInput();
+		}
+	}
+
+	// get route
+	public function logout()
+	{
+		Auth::logout();
+		Session::flash('successFlash', 'Logout successful!');
+		return Redirect::to('/');
+	}
+//===================== End Authentication Controllers ====================
 
 	public function updateAll()
 	{
