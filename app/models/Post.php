f7ac0688411a6107dbb9e9527d986fc4832e64e6
Jamie Haskell <jmehaskell@gmail.com>
2015-05-06 13:28:05 -0500

    added a comment to user() method

--- app/models/Post.php
+++ app/models/Post.php
@@ -11,12 +11,20 @@
 		];
 
 
-		// Store all post titles as a sluggified version of themselves in the 'slug' column
+		// Mutator that stores all post titles as a sluggified version of themselves in the 'slug' column
 		// This will create a duplicate of each post's title, connected by hyphens
 		// for use as a url appendage to direct link to each post (i.e. "This Blog Title" will also be stored as "this-blog-title")
 		public function setSlugAttribute($value)
 		{
 		    $this->attributes['slug'] = Str::slug($value);
+		}
+
+		public function user()
+		{
+			// connects each post to its user
+			// the first parameter is the User class, the second is the foreign-key, and the third is the local key that the foreign key references on the posts table
+			// second and third parameters are only needed if not using primary key "id" in the users table
+			return $this->belongsTo('User', 'user_id', 'user_id');
 		}
 
 	}
