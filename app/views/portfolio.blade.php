712bb907c80b4044bf6d542c46fc29904639fc30
Jamie Haskell <jmehaskell@gmail.com>
2015-05-10 23:12:36 -0500

    added jquery to add ACTIVE class to navbar on page-load

--- app/views/portfolio.blade.php
+++ app/views/portfolio.blade.php
@@ -1,5 +1,61 @@
 @extends('layouts.master')
 
 @section('portfolio')
-	<h1>This is my portfolio page</h1>
+	<h1>My Portfolio</h1>
+	<section id="main-content">
+		<div class="row">
+			<div class="small-12 columns">
+				<h3>Website Development:</h3>
+			</div>
+		</div>
+		<div class="row">
+			<div class="panel large-12 columns">
+				<div class="row">
+					<div class="large-4 columns">
+						<a href='#'><img class='thumbnail' src="http://placehold.it/300x200"/></a>
+						<h5>Capstone Project</h5>
+						<p>A blankity-blank written in Laravel.</p>
+					</div>
+					<div class="large-4 columns">
+						<a href='#'><img class='thumbnail' src="/img/MileMatrix.png"/></a>
+						<h5>Mile Matrix</h5>
+						<p>A credit card rewards program recommendation engine in Laravel.</p>
+					</div>
+					<div class="large-4 columns">
+						<a href='#'><img class='thumbnail' src="/img/Adlister.png"/></a>
+						<h5>Adlister</h5>
+						<p>A Craigslist-clone written in PHP.</p>
+					</div>
+				</div>
+			</div>
+			<div class='large-12 columns' style="height:25px;">
+			</div>
+			<div class="panel large-12 columns">
+				<h3>Web Applications:</h3>
+				<div class="row">
+					<div class="large-4 columns">
+						<a href='#'><img src="/img/Tremors_small.png"/></a>
+						<h5>Whack-a-worm Game</h5>
+						<p>A whack-a-mole style game created with jQuery.</p>
+					</div>
+					<div class="large-4 columns">
+						<a href='#'><img src="/img/SimonSays-small.png"/></a>
+						<h5>Simon Says Game</h5>
+						<p>A sequence memory game created with JavaScript.</p>
+					</div>
+					<div class="large-4 columns">
+						<a href='#'><img src="/img/Calculator_small.png"/></a>
+						<h5>JavaScript Calculator</h5>
+						<p>Written in, you guessed it, JavaScript!</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>	
+	<script>
+    	$(document).ready(function(){
+    		// $('.active').removeClass('active');
+    		$('#portfolio').addClass('active');
+    	});
+    </script>
 @stop
--- app/views/resume.blade.php
+++ app/views/resume.blade.php
@@ -1,5 +1,53 @@
 @extends('layouts.master')
 
 @section('resume')
-	<h1>This is my resume page</h1>
+	<h1>Resume</h1>
+	<section>
+      <!-- Three columns of text below the carousel -->
+      	<div id='resumeMenu' class="row">
+        	<div class="large-4 text-center columns">
+          		<div class="iconCircle">
+            		<a href="resume_edu.html">
+            			<i id="cap" class="fa fa-graduation-cap fa-5x"></i>
+            		</a>
+          		</div>
+          		<a href="resume_edu.html" class="bodyLinks">
+          			<h2 class="extraMargin">Education</h2>
+          		</a>
+          		<p class="extraMargin">See where I earned my degrees and received my training!</p>
+          		<!-- <p><a class="btn btn-default" href="/resume_edu.html" role="button">View details »</a></p> -->
+        	</div><!-- /.col-lg-4 -->
+        	<div class="large-4 text-center columns">
+          		<div class="iconCircle">
+            		<a href="resume_lang.html">
+            			<i id ="code" class="fa fa-code fa-5x "></i>
+            		</a>
+          		</div>
+          		<a href="resume_lang.html" class="bodyLinks">
+          			<h2>Programming Languages</h2>
+          		</a>
+          		<p>If I don't know the exact language you need, not to worry. I'll learn it!</p>
+          		<!-- <p><a class="btn btn-default" href="#" role="button">View details »</a></p> -->
+	        </div><!-- /.col-lg-4 -->
+	        <div class="large-4 text-center columns">
+	          	<div class="iconCircle">
+	            	<a href="resume_work.html">
+	            		<i id="clock" class="fa fa-clock-o fa-5x "></i>
+	            	</a>
+	          	</div>
+	          	<a href="resume_work.html" class="bodyLinks">
+	          		<h2 class="extraMargin">Work History</h2>
+	          	</a>
+	          	<p class="extraMargin">This might surprise you...</p>
+	          	<!-- <p><a class="btn btn-default" href="#" role="button">View details »</a></p> -->
+	        </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->
+    </section>
+
+    <script>
+    	$(document).ready(function(){
+    		$('.active').removeClass('active');
+    		$('#resume').addClass('active');
+    	});
+    </script> 
 @stop
